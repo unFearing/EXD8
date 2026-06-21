ab9643b18d872dd2148af9aa90b2a79571f0b51c
unFearing <31084866+unFearing@users.noreply.github.com>
2026-06-20 23:52:21 -0700

    Use explicit Azure Functions entrypoint for API registration

---
+++ api/src/index.ts
@@ -0,0 +1,8 @@
+import "./functions/health/get.js";
+import "./functions/mechs/create.js";
+import "./functions/mechs/getById.js";
+import "./functions/mechs/hierarchy.js";
+import "./functions/mechs/list.js";
+import "./functions/mechs/upsert.js";
+import "./functions/matchNights/create.js";
+import "./functions/matchNights/getById.js";
